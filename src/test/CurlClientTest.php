11723f0bfc588c9f92295ca8a712718dc1595dd8
tundraka <tundraka@gmail.com>
2015-07-19 17:53:06 -0500

    Matching src directories

--- src/test/CurlClientTest.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-class CurlClientTest extends PHPUnit_Framework_TestCase {
-}
---
+++ src/test/utils/CurlClientTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Utils;
+
+class CurlClientTest extends \PHPUnit_Framework_TestCase {
+    use \InterNations\Component\HttpMock\PHPUnit\HttpMockTrait;
+
+    public static function setUpBeforeClass() {
+        static::setUpHttpMockBeforeClass('8082', 'localhost');
+    }
+
+    public static function tearDownAfterClass() {
+        static::tearDownHttpMockAfterClass();
+    }
+
+    public function setUp() {
+        $this->setUpHttpMock();
+    }
+
+    public function tearDown() {
+        $this->tearDownHttpMock();
+    }
+}
